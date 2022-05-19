8db7997fed227a18d868a81af2bb44c94fc347b5
s-hayashi <s-hayashi@creationline.com>
2022-05-19 12:49:20 +0900

    Added: index

---
+++ docusaurus/src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <main>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--primary button--lg"
+            to="/docs/intro">Getting started</Link>
+        </div>
+      </main>
+    </Layout>
+  );
+}
